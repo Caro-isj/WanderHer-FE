6a30ff46fcd70ebc339724122fc62877b61bfa87
Alexandra Busson <alexandra_b@live.fr>
2024-04-05 09:52:39 +0200

    before merging day03

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from "react";
-import { useState } from "react";
-
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import SignUp from "./components/SignUp";
@@ -33,5 +31,4 @@ function App() {
   );
 }
 
-export default App;
 export default App;
